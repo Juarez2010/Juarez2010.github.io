99a216816f6c95b2c4e86a4c8b1445f09f3c2df1
Juarez2010 <oro10jua@gmail.com>
2022-02-08 09:45:39 -0800

    moving files

---
+++ js/page.js
@@ -0,0 +1,18 @@
+function testlol() {
+
+document.getElementById('buttontest').innerHTML = 'You found a secret?';
+
+}
+
+
+function oldlinkclick {
+
+document.getElementById('old-site').innerHTML = 'You clicked the link.';
+
+}
+
+function amongsus {
+
+document.getElementbyId('sus').innerHTML = 'amogus';
+
+}
