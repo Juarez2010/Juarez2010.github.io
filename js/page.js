7dadd550b59d73ff0346646a4c8bd9a6170d07b3
Juarez2010 <juarez2010yt@gmail.com>
2022-03-04 09:19:14 -0800

    lol

--- js/page.js
+++ js/page.js
@@ -15,6 +15,8 @@ document.getElementById("testlink").innerHTML = "You clicked the link.";
 
 function amongsus() {
 
+  window.open("https://juarez2010.github.io/website-v1/gif");
+ 
 document.getElementbyId("sus").innerHTML = "amogus";
 
 }
