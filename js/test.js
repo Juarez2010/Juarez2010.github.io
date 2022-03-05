861d3ee6bb2c57f3e790c8b750737e658b13bb9d
Juarez2010 <juarez2010yt@gmail.com>
2022-03-05 14:03:02 -0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-javascript:((function(){var a,b,c;c="javascript:document.write('<!DOCTYPE html>
+document.write('<!DOCTYPE html>
 <html>
 <title>Website v2?</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -56,4 +56,4 @@ javascript:((function(){var a,b,c;c="javascript:document.write('<!DOCTYPE html>
 	        <br>
   <a id="old-site" onclick="oldlinkclick()" href="https://juarez2010.github.io/website-v1" target="_blank">Visit the old site!</a><!-- visit it if u want lol-->
 </body>
-</html>",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","rusic-modal"),b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+</html>
